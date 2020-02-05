dca8147f46cc140bc9bf0ed27f67cb178d71ecdb
jmausolf <jgm346@gmail.com>
2020-02-05 13:44:45 -0600

    updating model analysis

--- analysis/glmer_table_lag_models.R
+++ analysis/glmer_table_lag_models.R
@@ -1,17 +1,27 @@
 
-#Set Model List
-#models <- list(m1ld, m2ld, m3ld, m4ld)
-#models <- list(m1lr, m2lr, m3lr, m4lr)
-#models <- list(m1ld2, m2ld2, m3ld2, m4ld2)
+
 
 #Set Options
-models <- list(m1lr, m2lr, m3lr, m4lr)
-ttitle = "Mixed Effects Models of Adding a New Board Member (Republican), Odds Ratios Displayed"
-dvar = "Pr\\{New Board Member: Republican\\}"
-outfile = "glmer_models_republican_lag1.tex"
+# models <- list(mlr_2, mlr_4, mlr_6, mlr_8)
+# ttitle = "Mixed Effects Models of Adding a New Board Member (Republican), Odds Ratios Displayed"
+# dvar = "Pr\\{New Board Member: Republican\\}"
+# outfile = "output/tables/glmer_models_republican_multilag.tex"
+
+
+
+#Set Options
+models <- list(mld_2, mld_4, mld_6, mld_8)
+ttitle = "Mixed Effects Models of Adding a New Board Member (Democrat), Odds Ratios Displayed"
+dvar = "Pr\\{New Board Member: Democrat\\}"
+outfile = "output/tables/glmer_models_democrat_multilag.tex"
+
+
+
+
+
 
 #asr
-r = 34
+r = 16
 d = 3
 
 
@@ -19,6 +29,7 @@ d = 3
 
 re1l = "Firm"
 re2l = "Year"
+re3l = "Lag Year"
 
 #Add Insert Row Function
 insertrow <- function(existingDF, newrow, r) {
@@ -56,30 +67,10 @@ tables <- stargazer3(models,
                      style = "asr",
                      title = ttitle,
                      dep.var.labels   = dvar,
-                     covariate.labels = c("\\textit{Boards and Firm Politics} \\\\Board Member Swap",
+                     covariate.labels = c("Board Member Swap",
                                           "Republican Board",
                                           "Amphibious Firm",
                                           "Republican Firm",
-                                          
-                                          "\\\\ \\textit{Board Features} \\\\ Median Age",
-                                          "Proportion Female",
-                                          "Proportion Black or Hispanic",
-                                          "Proportion Minority",
-                                          "Proportion Non-US",
-                                          "Median Outside Board Ties",
-                                          
-                                          "\\\\ \\textit{Firm Sectors} \\\\ Capital Goods",
-                                          "Conglomerates",
-                                          "Consumer Cyclical",
-                                          "Consumer Goods",
-                                          "Consumer/Non-Cyclical",
-                                          "Energy",
-                                          "Financial",
-                                          "Healthcare",
-                                          "Services",
-                                          "Technology",
-                                          "Transportation",
-                                          "Utilities",
                                           "Constant")
                      )
 
@@ -96,47 +87,59 @@ tables <- insertrow(tables,randomeffect,r+1)
 #Get number of RE's
 num.re1.m1 <- sapply(ranef(m1),nrow)[1]
 num.re2.m1 <- sapply(ranef(m1),nrow)[2]
+num.re3.m1 <- sapply(ranef(m1),nrow)[3]
 
 num.re1.m2 <- sapply(ranef(m2),nrow)[1]
 num.re2.m2 <- sapply(ranef(m2),nrow)[2]
+num.re3.m2 <- sapply(ranef(m2),nrow)[3]
 
 num.re1.m3 <- sapply(ranef(m3),nrow)[1]
 num.re2.m3 <- sapply(ranef(m3),nrow)[2]
+num.re3.m3 <- sapply(ranef(m3),nrow)[3]
 
 num.re1.m4 <- sapply(ranef(m4),nrow)[1]
 num.re2.m4 <- sapply(ranef(m4),nrow)[2]
+num.re3.m4 <- sapply(ranef(m4),nrow)[3]
 
 
 #Get Variance
 var.re1.m1 <- as.data.frame(VarCorr(m1))$vcov[1]
 var.re2.m1 <- as.data.frame(VarCorr(m1))$vcov[2]
+var.re3.m1 <- as.data.frame(VarCorr(m1))$vcov[3]
 
 var.re1.m2 <- as.data.frame(VarCorr(m2))$vcov[1]
 var.re2.m2 <- as.data.frame(VarCorr(m2))$vcov[2]
+var.re3.m2 <- as.data.frame(VarCorr(m2))$vcov[3]
 
 var.re1.m3 <- as.data.frame(VarCorr(m3))$vcov[1]
 var.re2.m3 <- as.data.frame(VarCorr(m3))$vcov[2]
+var.re3.m3 <- as.data.frame(VarCorr(m3))$vcov[3]
 
 var.re1.m4 <- as.data.frame(VarCorr(m4))$vcov[1]
 var.re2.m4 <- as.data.frame(VarCorr(m4))$vcov[2]
+var.re3.m4 <- as.data.frame(VarCorr(m4))$vcov[3]
 
 
 #make RE row entries
 num.re1 <- paste0(re1l, "s &", num.re1.m1, "&", num.re1.m2, "&", num.re1.m3, "&", num.re1.m4, "\\\\")
-var.re1 <- paste0(re1l, " Variance &", round(var.re1.m1, d), "&", round(var.re1.m2, d), "&", round(var.re1.m3, d), "&", round(var.re1.m4, d), "\\\\")
-
 num.re2 <- paste0(re2l, "s &", num.re2.m1, "& ", num.re2.m2, "& ", num.re2.m3, "& ", num.re2.m4, "\\\\")
+num.re3 <- paste0(re3l, "s & [1,", num.re3.m1, "] & [1,", num.re3.m2, "] & [1,", num.re3.m3, "] & [1,", num.re3.m4, "] \\\\")
+
+
+var.re1 <- paste0(re1l, " Variance &", round(var.re1.m1, d), "&", round(var.re1.m2, d), "&", round(var.re1.m3, d), "&", round(var.re1.m4, d), "\\\\")
 var.re2 <- paste0(re2l, " Variance &", round(var.re2.m1, d), "&", round(var.re2.m2, d), "&", round(var.re2.m3, d), "&", round(var.re2.m4, d), "\\\\")
+var.re3 <- paste0(re3l, " Variance &", round(var.re3.m1, d), "&", round(var.re3.m2, d), "&", round(var.re3.m3, d), "&", round(var.re3.m4, d), "\\\\")
 
 #add rows
 #tables <- insertrow(tables,num.re1, r+3)
 tables <- insertrow(tables,var.re1, r+2)
-#tables <- insertrow(tables,newline, r+5)
-#tables <- insertrow(tables,num.re2, r+6)
 tables <- insertrow(tables,var.re2, r+3)
-tables <- insertrow(tables, hline2, r+4)
-tables <- insertrow(tables,newline, r+5)
-tables <- insertrow(tables,num.re1, r+7)
-tables <- insertrow(tables,num.re2, r+8)
+tables <- insertrow(tables,var.re3, r+4)
+tables <- insertrow(tables, hline2, r+5)
+tables <- insertrow(tables,newline, r+6)
+
+tables <- insertrow(tables,num.re1, r+8)
+tables <- insertrow(tables,num.re2, r+9)
+tables <- insertrow(tables,num.re3, r+10)
 
 write.table(tables,file=outfile,sep="",row.names= FALSE,na="", quote = FALSE, col.names = FALSE)
